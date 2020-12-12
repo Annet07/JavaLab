5bafe1ff79013dd12b32cc3451fee0d486478c92
Annet07 <BudrevichAnya@gmail.com>
2020-12-12 17:11:16 +0300

    Hometask AnnotationsSource

--- 09.AnnotationsSOURCE/src/main/java/HtmlInput.java
+++ 09.AnnotationsSOURCE/src/main/java/HtmlInput.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class HtmlInput {
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.SOURCE)
+@Target(ElementType.FIELD)
+public @interface HtmlInput {
+    String type() default "text";
+    String name() default "";
+    String placeholder() default "";
 }
--- 09.AnnotationsSOURCE/src/main/java/User.java
+++ 09.AnnotationsSOURCE/src/main/java/User.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class User {
+
+@HtmlForm(method = "post", action = "/users")
+public class User {
+    @HtmlInput(name = "nickname", placeholder = "Ваш ник")
+    private String nickname;
+    @HtmlInput(name = "email", type = "email", placeholder = "Ваша почта")
+    private String email;
+    @HtmlInput(name = "password", type = "password", placeholder = "Ваш пароль")
+    private String password;
 }
